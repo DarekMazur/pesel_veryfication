bbb6f8c3d2adbbbd35e42b9e20cac00d7eae05a5
Darek Mazur <dev@darekmazur.it>
2023-09-18 16:23:35 +0200

    Add date of birth validation

--- src/methods/getBirthday.js
+++ src/methods/getBirthday.js
@@ -54,8 +54,38 @@ export const getBirthDay = (date) => {
       return monthName
     }
 
-    return month()
+    return month(`${getCentury(dateDetails[2])}`)
   }
 
-  return `${dateDetails[4] !== '0' || ''}${dateDetails[5]} ${getMonth(dateDetails[2] + dateDetails[3])} ${getCentury(dateDetails[2])}`
+  const getMonthNumber = (monthNumber) => {
+    const month = () => {
+      const monthFullNumber = monthNumber.split('')[0] % 2 === 0 ? (
+        '0' + monthNumber.split('')[1]
+      ) : (
+        '1' + monthNumber.split('')[1]
+      )
+
+      return monthFullNumber
+    }
+
+    return month(`${getCentury(dateDetails[2])}`)
+  }
+
+  const today = new Date()
+  const birthDay = new Date(`${getCentury(dateDetails[2])}/${getMonthNumber(dateDetails[2] + dateDetails[3])}/${dateDetails[4]}${dateDetails[5]}`)
+
+  try {
+    if (Date.prototype.toString.call(birthDay) !== 'Invalid Date') {
+      const timeGap = (birthDay.getTime() - today.getTime()) / (1000 * 3600 * 24)
+      if (timeGap > 0) {
+        return `${dateDetails[4] !== '0' || ''}${dateDetails[5]} ${getMonth(dateDetails[2] + dateDetails[3])} ${getCentury(dateDetails[2])}`
+      } else {
+        throw new Error('Date of birth must be before today')
+      }
+    } else {
+      throw new Error('Date of birth format is incorrect')
+    }
+  } catch(err) {
+    return err.message
+  }
 }
