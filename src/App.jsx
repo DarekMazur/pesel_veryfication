c863649bfaaf0d1634f3f440f7934900c4233826
Darek Mazur <dev@darekmazur.it>
2023-10-02 20:18:18 +0200

    Create styled wrapper components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { getPeselData } from "./methods/getPeselData"
 import {getBirthDay} from "./methods/getBirthday.js";
 import {getSex} from "./methods/getSex.js";
 import {useState} from "react";
+import {Wrapper} from './components/Wrapper/Wrapper.styles.js'
 
 const App = () => {
   const initialState = {
@@ -26,18 +27,17 @@ const App = () => {
     return (
       <>
         <h1>Hello world!</h1>
-        <div>
+        <Wrapper>
           <form onSubmit={handleSubmit}>
             <input id='pesel' name='pesel' value={pesel} onChange={handleInputChange}/>
             <label htmlFor='pesel'>PESEL number</label>
             <button type='submit'>Check</button>
           </form>
-        </div>
-        <div>
+
           <p>{input ? `${input}: ${getPeselData(input) === true ? 'PESEL is valid' : getPeselData(input)}` : initialState.validationStatus}</p>
           <p>{input && getPeselData(input) === true ? `Day of birth: ${getBirthDay(input.split('').slice(0, 6).join(''))}` : initialState.birthday}</p>
           <p>{input && getPeselData(input) === true ? `Gender: ${getSex(input.split('')[input.split('').length - 2])}` : initialState.sex}</p>
-        </div>
+        </Wrapper>
       </>
     )
 }
