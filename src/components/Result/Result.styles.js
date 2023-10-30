69580a83f5fca5e6b0a48ab2c314b7eb935f3b93
Darek Mazur <dev@darekmazur.it>
2023-10-30 04:03:49 +0100

    Style Result element

--- src/components/Result/Result.styles.js
+++ src/components/Result/Result.styles.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Result = styled.p`
+	width: 380px;
+	font-size: 18px;
+		
 	span {
 		font-weight: 800;	
     color: ${({isValid}) => isValid ? 'green' : 'red'}
