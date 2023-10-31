8026f74579b602ab6bcca8aadd329087d2f30ab6
Darek Mazur <dev@darekmazur.it>
2023-10-31 05:40:14 +0100

    Style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {H1} from "../H1/H1.styles.js";
+import {StyledHeader} from "./Header.styles.js";
 
 const Header = () => {
 	return(
-		<header>
-			<H1>Hello world!</H1>
-		</header>
+		<StyledHeader>
+			<H1>Check PESEL</H1>
+		</StyledHeader>
 	)
 }
 
--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,25 +1,25 @@
-import { createGlobalStyle } from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-    html {
-        box-sizing: border-box;
-        font-size: 62.5%;
-    }
+		html {
+				box-sizing: border-box;
+				font-size: 62.5%;
+		}
 
-    body {
-        font-family: ${({theme}) => theme.fonts.main};
-		    margin: 0;
-		    
-		    #root {
-            min-height: 100vh;
-            display:flex;
-				    flex-direction:column;
-				    justify-content: space-between;
-				    margin: 0;
-		    }
-    }
+		body {
+				font-family: ${({theme}) => theme.fonts.main};
+				margin: 0;
 
-    *, *::after, *::before {
-        box-sizing: inherit;
-    }
+				#root {
+						min-height: 100vh;
+						display: flex;
+						flex-direction: column;
+						justify-content: space-between;
+						margin: 0;
+				}
+		}
+
+		*, *::after, *::before {
+				box-sizing: inherit;
+		}
 `;
